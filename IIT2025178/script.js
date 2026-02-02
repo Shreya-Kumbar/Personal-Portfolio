928a3a2324e3a98172a10269b9d5ab3169364799
Shreya-Kumbar <shreyarkumbar@gmail.com>
2026-02-02 07:57:22 +0530

    Added files

---
+++ IIT2025178/script.js
@@ -0,0 +1,13 @@
+window.alert("You are being directed to My Functions.");
+
+function myfunc() {
+    document.getElementById('demo').innerHTML = "plllllllllllllllllllllll"
+    console.log(5+7)
+}
+
+let clicked = 0;
+
+function clickCounter() {
+    clicked++;
+    document.getElementById('counter').innerHTML = "Clicked " + clicked + " times";
+}
