21cd688d6d812268e90f9cf17f2eee99fb5d9989
Shreya-Kumbar <shreyarkumbar@gmail.com>
2026-02-02 20:51:35 +0530

    Updated JS

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-window.alert("You are being directed to My Functions.");
+window.alert("You are being directed to My JavaScript Functions.");
 
 function myfunc() {
     document.getElementById('demo').innerHTML = "plllllllllllllllllllllll"
@@ -10,4 +10,21 @@ let clicked = 0;
 function clickCounter() {
     clicked++;
     document.getElementById('counter').innerHTML = "Clicked " + clicked + " times";
+}
+
+function checkEvenOdd(number) {
+
+    let input = document.getElementById('number_input').value;
+    let n = Number(document.getElementById('number_input').value);
+
+    if (input === ""){
+        document.getElementById('result').innerText = "Please enter a number...";
+        return;
+    }
+
+    if (n % 2 === 0)
+        document.getElementById('result').innerText = n + " is Even";
+    else
+        document.getElementById('result').innerText = n + " is Odd";
+
 }
